4253a6120e99c5a0b6c69802ba39fba0a5f8e285
Raquel Penteado <raquelpenteado4@gmail.com>
2023-08-09 21:36:58 -0300

    Atualizações: inserindo rotas, controllers, models e nova tabela através do migrate

---
+++ resources/views/admin/supports/index.blade.php
@@ -0,0 +1,1 @@
+<h1>Listagem dos Suportes</h1>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,13 @@
 <?php
 
+use App\Http\Controllers\Admin\{SupportController};
+use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('sites/contato', [SiteController::class, 'contact']);
