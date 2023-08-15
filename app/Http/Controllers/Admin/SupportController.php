b913d6f928fb233f24bcd3adff4845f6113c45bc
Raquel Penteado <raquelpenteado4@gmail.com>
2023-08-15 16:00:15 -0300

    Validação: Formulário de "Nova Dúvida" validado

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateSupport;
 use App\Models\Support;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -25,11 +26,11 @@ class SupportController extends Controller
         return view('admin/supports/create');
     }
 
-    public function store(Request $request, Support $support) {
+    public function store(StoreUpdateSupport $request, Support $support) {
         $data = $request->all();
         $data['status'] = 'a';
 
-        $support = $support->create($data);
+        $support->create($data);
 
         return redirect()->route('supports.index');
     }
--- resources/views/admin/supports/create.blade.php
+++ resources/views/admin/supports/create.blade.php
@@ -1,8 +1,14 @@
 <h1>Nova Dúvida</h1>
 
+@if ($errors->any())
+    @foreach ($errors->all() as $error)
+        {{ $error }}
+    @endforeach
+@endif
+
 <form action="{{ route('supports.store') }}" method="POST">
     @csrf()
-    <input type="text" name="subject" placeholder="Assunto">
-    <textarea name="body" cols="30" rows="5" placeholder="Descrição"></textarea>
+    <input type="text" name="subject" placeholder="Assunto" value="{{ old('subject') }}">
+    <textarea name="body" cols="30" rows="5" placeholder="Descrição">{{ old('body') }}</textarea>
     <button type="submit">Enviar</button>
 </form>
